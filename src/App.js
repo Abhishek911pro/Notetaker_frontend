ff3150b0a5c14f66639d1e611fae06d3320fdb03
Abhishek_kumar <Abhishku611@gmail.com>
2023-05-03 21:16:51 +0530

    solved router error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+
 
 import './App.css';
 import Header from './components/Header'
+import NotesListPage from './pages/NotesListPages'
+import NotePage from './pages/NotePage'
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      my app
-    </div>
+    <Router>
+      
+        <div className="App">
+        <Header/>
+        <Routes>
+        <Route path="/" exact Component={NotesListPage}/>
+        </Routes>
+        <Routes>
+        <Route path="/note/:id" Component={NotePage}/>
+        </Routes>
+        </div>
+        
+    </Router>
+    
   );
 }
 
